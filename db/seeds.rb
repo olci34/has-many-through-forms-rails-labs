d2a4f9ebc89ccec8fb9516ade821d87d7e2db711
github username <github email address>
2021-03-17 22:52:06 -0400

    commit tree 6d114aea452b899dd47471a6ba119a49485efc25

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+user1 = User.create(username: "johnny", email: "j@g.com")
+user2 = User.create(username: "heather", email: "2ad@s.com")
+
+ctg1 = Category.create(name: "Drama")
+ctg2 = Category.create(name: "Health")
+
+post1 = user1.posts.create(title: "johnny posts", content: "Johnny posts content")
+post2 = user2.posts.create(title: "heather posts", content: "heather posts content")
+
+post1.categories = [ctg1, ctg2]
+post2.categories = [ctg2]
+post1.save
+post2.save
+user1.comments.create(content: "Johnny comments", post_id:3)
+
+
+
